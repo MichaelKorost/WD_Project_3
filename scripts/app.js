232dbc7594138c740066d8d33fb777cfe5b6ad71
Michael <michaelkorostilev@gmail.com>
2022-08-28 16:50:42 +0300

    made adjustments to nav

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,4 @@
+const navBar = document.querySelector('.nav-bar')
 const navMenu = document.querySelector(".nav__menu");
 const navMenuImg = document.querySelector(".nav__menu img");
 const navMenuClose = document.querySelector(".nav__menu-close");
@@ -7,6 +8,7 @@ const navItem = document.querySelectorAll(".nav__item");
 navMenu.addEventListener("click", openNavMenu);
 
 function openNavMenu() {
+  // navBar.classList.toggle('nav-bar--active')
   navMenuImg.classList.toggle("nav__menu--deactive");
   navMenuClose.classList.toggle("nav__menu-close--active");
   navTitle.classList.toggle("nav__title--active");
