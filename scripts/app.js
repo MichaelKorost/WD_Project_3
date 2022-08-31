2d02272aa0d70d82257d015067e7f7bf69a7c0d1
Michael <michaelkorostilev@gmail.com>
2022-08-31 12:24:14 +0300

    added navbar functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,38 @@
-const navBar = document.querySelector('.nav-bar')
+const page = document.querySelector(".page");
+const navBar = document.querySelector(".nav-bar");
 const navMenu = document.querySelector(".nav__menu");
 const navMenuImg = document.querySelector(".nav__menu img");
 const navMenuClose = document.querySelector(".nav__menu-close");
 const navTitle = document.querySelector(".nav__title");
+const navTitleLogo = document.querySelector(".nav__title a");
 const navItem = document.querySelectorAll(".nav__item");
 
+navMenu.addEventListener("click", toggleNavMenu);
 
-
-navMenu.addEventListener("click", openNavMenu);
-
-function openNavMenu() {
-  // navBar.classList.toggle('nav-bar--active')
+function toggleNavMenu() {
+  navBar.classList.toggle("nav--active");
   navMenuImg.classList.toggle("nav__menu--deactive");
   navMenuClose.classList.toggle("nav__menu-close--active");
   navTitle.classList.toggle("nav__title--active");
+  page.classList.toggle("page--scroll-lock");
   for (let i = 0; i < navItem.length; i++) {
     navItem[i].classList.toggle("nav__item--active");
   }
 }
+
+for (let i = 0; i < navItem.length; i++) {
+  navItem[i].addEventListener("click", toggleNavMenu);
+}
+
+navTitleLogo.addEventListener("click", closeNavMenu);
+
+function closeNavMenu() {
+  console.log("function close working");
+  if (navBar.classList.contains("nav--active")) {
+    console.log("closing");
+    toggleNavMenu();
+  }
+}
+
+//create toggle navMenu
+//create nav dynamically
