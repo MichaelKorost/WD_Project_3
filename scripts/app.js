6ecfdbf8b05887a49176effd415c756a28adafdc
Michael <michaelkorostilev@gmail.com>
2022-08-31 15:20:01 +0300

    navbar is dynamically created and is fully functional

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,58 @@
+const navBar = document.querySelector(".nav-bar"); //getting navBar to append into
+const sections = document.querySelectorAll("section"); //getting sections to name each navBar item
+
+//creating navbar dynamically
+const ul = document.createElement("ul");
+ul.classList.add("nav");
+const divLogo = document.createElement("div");
+divLogo.classList.add("nav__title");
+const divTitle = document.createElement("a");
+divTitle.setAttribute("href", "#header");
+divTitle.innerHTML = "PinkPink<span class='nav__title-dot'>.</span>";
+const divButton = document.createElement("button");
+divButton.classList.add("nav__menu");
+const divButtonImg = document.createElement("img");
+divButtonImg.setAttribute("src", "./assets/icons/hamburger-menu.png");
+divButtonImg.setAttribute("alt", "menu");
+const divButtonImgClose = document.createElement("img");
+divButtonImgClose.setAttribute(
+  "src",
+  "./assets/icons/close hamburger-menu.png"
+);
+divButtonImgClose.setAttribute("alt", "menu");
+divButtonImgClose.classList.add("nav__menu-close");
+
+divButton.appendChild(divButtonImg);
+divButton.appendChild(divButtonImgClose);
+
+divLogo.appendChild(divTitle);
+divLogo.appendChild(divButton);
+
+ul.appendChild(divLogo);
+navBar.appendChild(ul);
+
+createListItem(); //for each section creating a nav item
+function createListItem() {
+  for (let i = 1; i < sections.length; i++) {
+    const navListItem = document.createElement("li");
+    navListItem.classList.add("nav__item");
+
+    const navListItemLink = document.createElement("a");
+    navListItemLink.setAttribute("href", `#${sections[i].id}`);
+    navListItemLink.textContent = sections[i].getAttribute("data-nav");
+
+    const navListItemImg = document.createElement("img");
+    navListItemImg.setAttribute("src", "./assets/icons/nav-mobile-arrow.png");
+    navListItemImg.setAttribute("alt", "arrow");
+    navListItemImg.classList.add("nav__arrow");
+
+    navListItem.appendChild(navListItemLink);
+    navListItem.appendChild(navListItemImg);
+    ul.appendChild(navListItem);
+  }
+}
+//adding relavent elements to make navBar functional
 const page = document.querySelector(".page");
-const navBar = document.querySelector(".nav-bar");
 const navMenu = document.querySelector(".nav__menu");
 const navMenuImg = document.querySelector(".nav__menu img");
 const navMenuClose = document.querySelector(".nav__menu-close");
@@ -20,19 +73,14 @@ function toggleNavMenu() {
   }
 }
 
-for (let i = 0; i < navItem.length; i++) {
-  navItem[i].addEventListener("click", toggleNavMenu);
-}
-
 navTitleLogo.addEventListener("click", closeNavMenu);
 
+navItem.forEach((item) => {
+  item.addEventListener("click", closeNavMenu);
+});
+
 function closeNavMenu() {
-  console.log("function close working");
   if (navBar.classList.contains("nav--active")) {
-    console.log("closing");
     toggleNavMenu();
   }
 }
-
-//create toggle navMenu
-//create nav dynamically
